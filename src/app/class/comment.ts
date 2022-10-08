94fa8a481ba4b3a7fa160a78547d77474c7eda0a
sho-itsuka <sho.itsuka122@gmail.com>
2022-10-08 16:25:17 +0900

    class Userにプロパティの定義

--- src/app/class/comment.ts
+++ src/app/class/comment.ts
@@ -1,4 +1,4 @@
 export class Comment {
-  name!: string
+  name!:    string
   message!: string
 }
--- src/app/class/user.ts
+++ src/app/class/user.ts
@@ -1,2 +1,10 @@
 export class User {
+  // アイコンの名前の一文字目
+  initial: string
+
+  // ユーザーはインスタンス化する際にパラメータを2つ受け取る
+  constructor(public uid: number, public name: string) {
+    // 名前の最初の一文字目をsliceメソッドで受けとり、initialプロパティに保存
+    this.initial = name.slice(0, 1)
+  }
 }
