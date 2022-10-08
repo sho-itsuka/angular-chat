c4d1a664eaf869aabe444e7630da602d08c83f67
sho-itsuka <sho.itsuka122@gmail.com>
2022-10-08 15:08:41 +0900

    set up加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ac-root',
+  selector:    'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:  ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-chat';
